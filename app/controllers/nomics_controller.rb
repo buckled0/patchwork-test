d89d8f6b96080db95ff6d0e9412e8ec9ef23c8f3
Dan Buckle <danielbuckle31@gmail.com>
2021-09-30 14:51:27 +0100

    Add field extraction (Task 2)

--- app/controllers/nomics_controller.rb
+++ app/controllers/nomics_controller.rb
@@ -1,15 +1,19 @@
 class NomicsController < ApplicationController
   def get_ticker
-    render json: client.get_tickers(ticker_params)
+    tickers_fields = client.get_tickers(params["tickers"])
+
+    tickers_fields.map do |ticker|
+      ticker.select! do |k, v|
+        params["fields"].include?(k)
+      end
+    end if params["fields"].present?
+
+    render json: { tickers: tickers_fields }
   end
 
   private
 
   def client
     @client ||= NomicsApi::Client.new("35d3ca7c09f9794487f098683164e5ff5e8a574a")
-  end
-
-  def ticker_params
-    params.require(:tickers)
   end
 end
--- spec/controllers/nomics_controller_spec.rb
+++ spec/controllers/nomics_controller_spec.rb
@@ -17,5 +17,26 @@ RSpec.describe NomicsController do
         expect(subject.content_type).to eq("application/json; charset=utf-8")
       end
     end
+
+    context "when given ticker attributes" do
+      let(:params) do
+        {
+          tickers: ["BTC"],
+          fields: ["circulating_supply", "max_supply", "name", "symbol", "price"]
+        }
+      end
+
+      it { is_expected.to have_http_status :success }
+
+      it "has the correct keys" do
+        expect(response_body["tickers"].first.keys).to contain_exactly(
+          "circulating_supply", "max_supply", "name", "symbol", "price"
+        )
+      end
+    end
+  end
+
+  def response_body
+    JSON.parse(subject.body)
   end
 end
